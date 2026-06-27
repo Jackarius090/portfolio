e656ab8427156e8dd8dd16a774815def1d0c5b80
Jack Dickinson <jackdickinson090@hotmail.com>
2026-06-27 18:46:33 +0200

    fix cow again...

--- components/Greeting.tsx
+++ components/Greeting.tsx
@@ -5,11 +5,11 @@ import { useEffect } from "react";
 export default function Greeting() {
   useEffect(() => {
     console.log(`
-           .-------------------------------------.
-      (__) |Hey! Welcome to my portfolio website!|
+            .-------------------------------------.
+       (__) |Hey! Welcome to my portfolio website!|
 \\------(oo)/--------------------------------------'
-||    (__)                                
-||w--||
+ ||    (__)                                
+ ||w--||
 
 image by Unknown from https://www.asciiart.eu
     `);
